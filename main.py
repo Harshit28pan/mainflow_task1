a4c3e7e6ce2fe093d9241c3402a8ecb6e9731b6d
Harshit Pandey <134272856+Harshit28pan@users.noreply.github.com>
2024-07-02 20:39:45 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+my_list = [1, 2, 3, 4, 5]
+my_list.append(6)
+my_list.remove(3)
+my_list[1] = 10
+print("List:", my_list)
+
+my_dict = {'name': 'Alice', 'age': 30, 'city': 'Seattle'}
+my_dict['gender'] = 'female'
+del my_dict['age']
+my_dict['city'] = 'San Francisco'
+print("Dictionary:", my_dict)
+
+my_set = {1, 2, 3, 4, 5}
+my_set.add(6)
+my_set.discard(3)
+print("Set:", my_set)
+
+
